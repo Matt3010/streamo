e55a1838b7f3be60f2d307e951c7d282336334f8
Matteo Scanferla <m.scanferla@eggonemail.com>
2026-05-11 14:20:34 +0200

    Add autoplay script and enhance embed URL handling: implement `autoplay-embed.js` for autoplay functionality, refactor `seek-forwarder.js` to inject `autoplay` and `start` parameters into embed URLs, and update Nginx template to include new script.

--- scripts/seek-forwarder.js
+++ scripts/seek-forwarder.js
@@ -2,8 +2,20 @@
   // Read start time from outer URL (set by app.js via /player/movie/X?start=N)
   var params = new URLSearchParams(location.search);
   var startTime = parseInt(params.get('start'), 10);
+  var hasStartTime = startTime > 0;
 
-  if (!(startTime > 0)) return;
+  function decorateEmbedUrl(value) {
+    if (typeof value !== 'string' || value.indexOf('/embed/') === -1) return value;
+    if (value.indexOf('autoplay=1') === -1) {
+      var autoplaySep = value.indexOf('?') !== -1 ? '&' : '?';
+      value = value + autoplaySep + 'autoplay=1';
+    }
+    if (hasStartTime && value.indexOf('start=') === -1) {
+      var startSep = value.indexOf('?') !== -1 ? '&' : '?';
+      value = value + startSep + 'start=' + startTime;
+    }
+    return value;
+  }
 
   // Intercept iframe.src assignment to inject the start param into /embed/ URL
   var srcDescriptor = Object.getOwnPropertyDescriptor(HTMLIFrameElement.prototype, 'src') ||
@@ -16,11 +28,7 @@
       enumerable: srcDescriptor.enumerable,
       get: srcDescriptor.get,
       set: function(value) {
-        if (typeof value === 'string' && value.indexOf('/embed/') !== -1 && value.indexOf('start=') === -1) {
-          var sep = value.indexOf('?') !== -1 ? '&' : '?';
-          value = value + sep + 'start=' + startTime;
-        }
-        return origSet.call(this, value);
+        return origSet.call(this, decorateEmbedUrl(value));
       }
     });
   }
@@ -28,10 +36,8 @@
   // Also handle setAttribute("src", ...) for safety
   var origSetAttribute = Element.prototype.setAttribute;
   Element.prototype.setAttribute = function(name, value) {
-    if (this.tagName === 'IFRAME' && name === 'src' && typeof value === 'string' &&
-        value.indexOf('/embed/') !== -1 && value.indexOf('start=') === -1) {
-      var sep = value.indexOf('?') !== -1 ? '&' : '?';
-      value = value + sep + 'start=' + startTime;
+    if (this.tagName === 'IFRAME' && name === 'src' && typeof value === 'string') {
+      value = decorateEmbedUrl(value);
     }
     return origSetAttribute.call(this, name, value);
   };
