ae3dcc6ddf2bb3dd75cf5ad199539eb16fe8c6ec
Matteo Scanferla <m.scanferla@eggonemail.com>
2026-05-19 22:11:59 +0200

    test 8

--- scripts/seek-forwarder.js
+++ scripts/seek-forwarder.js
@@ -5,6 +5,14 @@
 
   if (!(startTime > 0)) return;
 
+  function withStart(value) {
+    if (typeof value !== 'string' || value.indexOf('/embed/') === -1 || value.indexOf('start=') !== -1) {
+      return value;
+    }
+    var sep = value.indexOf('?') !== -1 ? '&' : '?';
+    return value + sep + 'start=' + startTime;
+  }
+
   // Intercept iframe.src assignment to inject the start param into /embed/ URL
   var srcDescriptor = Object.getOwnPropertyDescriptor(HTMLIFrameElement.prototype, 'src') ||
                       Object.getOwnPropertyDescriptor(HTMLElement.prototype, 'src');
@@ -16,11 +24,7 @@
       enumerable: srcDescriptor.enumerable,
       get: srcDescriptor.get,
       set: function(value) {
-        if (typeof value === 'string' && value.indexOf('/embed/') !== -1 && value.indexOf('start=') === -1) {
-          var sep = value.indexOf('?') !== -1 ? '&' : '?';
-          value = value + sep + 'start=' + startTime;
-        }
-        return origSet.call(this, value);
+        return origSet.call(this, withStart(value));
       }
     });
   }
@@ -28,11 +32,30 @@
   // Also handle setAttribute("src", ...) for safety
   var origSetAttribute = Element.prototype.setAttribute;
   Element.prototype.setAttribute = function(name, value) {
-    if (this.tagName === 'IFRAME' && name === 'src' && typeof value === 'string' &&
-        value.indexOf('/embed/') !== -1 && value.indexOf('start=') === -1) {
-      var sep = value.indexOf('?') !== -1 ? '&' : '?';
-      value = value + sep + 'start=' + startTime;
+    if (this.tagName === 'IFRAME' && name === 'src') {
+      value = withStart(value);
     }
     return origSetAttribute.call(this, name, value);
   };
+
+  // The provider TV page ships a static <iframe src=".../embed/..."> in the
+  // HTML, so the browser may set it during parsing without passing through
+  // the JS setters above. Patch any already-present iframe once the DOM exists.
+  function patchExistingIframes() {
+    var frames = document.getElementsByTagName('iframe');
+    for (var i = 0; i < frames.length; i += 1) {
+      var frame = frames[i];
+      var current = frame.getAttribute('src') || frame.src;
+      var next = withStart(current);
+      if (next && next !== current) {
+        frame.setAttribute('src', next);
+      }
+    }
+  }
+
+  if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', patchExistingIframes, { once: true });
+  } else {
+    patchExistingIframes();
+  }
 })();
